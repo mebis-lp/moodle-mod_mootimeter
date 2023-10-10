0275d087bfe3fd5649cc66879d0c3b5a9144446f
Paola Maneggia <paola.maneggia@isb.bayern.de>
2023-10-10 17:10:53 +0200

    Cleanup helper_add_page

--- classes/helper_add_page.php
+++ classes/helper_add_page.php
@@ -64,54 +64,5 @@ class helper_add_page extends \mod_mootimeter\helper {
         $content = $OUTPUT->render_from_template('mod_mootimeter/add_new_page', $params);
 
         return $content;
-
-        $tools = [];
-        foreach ($enabledtools as $key => $tool) {
-            $tooltemp = [];
-            $tooltemp['pix'] = "tools/" . $tool . "/pix/" . $tool . ".svg";
-            $tooltemp['name'] = get_string('pluginname', 'mootimetertool_' . $tool);
-            $tooltemp['tool'] = $tool;
-            if (!empty($page)) {
-                $tooltemp['selected'] = ($tool == $page->tool) ? "selected" : "";
-            }
-            $tools[] = $tooltemp;
-        }
-
-        $editformparams = [
-            'cmid' => $cmid,
-            'pageid' => $pageid,
-            'title' => $paramtitle,
-            'sortorder' => $paramorder,
-            'tool' => $tool,
-            'tools' => $tools,
-            'accordionwrapperid' => 'settingswrapper',
-        ];
-
-        if (!empty($pageid)) {
-            $editformparams['title'] = $page->title;
-            $editformparams['sortorder'] = $page->sortorder;
-            $editformparams['question'] = $page->question;
-            $editformparams['toolsettings'] = $helper->get_tool_settings($page);
-            $editformparams['instancename'] = $page->title;
-        }
-
-        // $params['settings'] = $OUTPUT->render_from_template("mod_mootimeter/form_edit_page", $editformparams);
-
-        if (!empty($page)) {
-            $params['snippet_content_menu'] = $helper->get_content_menu($page);
-            $params['settings'] = $helper->get_col_settings($page);
-
-            $page->isNewPage = 'isNotNewPage';
-            $page->isNewPage = 'isNewPage';
-
-            $params['has_result'] = $helper->has_result_page($page);
-            if ($isresultpage) {
-                $params['pagecontent'] = $helper->get_rendered_page_result($page);
-            } else {
-                $params['pagecontent'] = $helper->get_rendered_page_content($page, $cm, false);
-            }
-        }
-
-        $params['isediting'] = $PAGE->user_is_editing();
     }
 }
