311ad8c621c53c8e21b519391c92d437cbe796a5
Peter Mayer <peter.mayer@isb.bayern.de>
2024-01-13 01:26:41 +0100

    Reload page on changing numbers form element

--- amd/src/handler_input_number.js
+++ amd/src/handler_input_number.js
@@ -1,5 +1,6 @@
 import {call as fetchMany} from 'core/ajax';
 import Log from 'core/log';
+import {execReloadPage as reloadPage} from 'mod_mootimeter/reload_page';
 
 export const init = (uniqueID) => {
     var up = document.getElementById('up_' + uniqueID);
@@ -87,5 +88,12 @@ const setINState = async(ajaxmethode, pageid, inputname, inputvalue, thisDataset
     const response = await execSetINState(ajaxmethode, pageid, inputname, inputvalue, thisDataset);
     if (response.code != 200) {
         Log.error(response.string);
+    }
+
+    if (response.code == 200) {
+        const queryString = window.location.search;
+        const urlParams = new URLSearchParams(queryString);
+        const cmid = urlParams.get('id');
+        reloadPage(pageid, cmid, thisDataset);
     }
 };
