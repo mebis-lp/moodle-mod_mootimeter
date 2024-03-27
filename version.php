df33bbce6d47946da1300ff31df6dc8fd805fa5b
Peter Mayer <peter.mayer@isb.bayern.de>
2024-03-27 10:33:53 +0100

    Remove support of moodle 4.2

--- version.php
+++ version.php
@@ -30,3 +30,4 @@ $plugin->release = '0.1.0';
 $plugin->version = 2023121102;
 $plugin->requires = 2022112800;
 $plugin->maturity = MATURITY_ALPHA;
+$plugin->supported = [403, 403]; // A range of branch numbers of supported moodle versions.
