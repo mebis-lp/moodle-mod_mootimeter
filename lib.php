3a5de43ac5cd0d7e8415dea87b97e58a1b6d6f09
Peter Mayer <peter.mayer@isb.bayern.de>
2024-05-07 13:42:45 +0200

    Remove code for grading

--- lib.php
+++ lib.php
@@ -31,8 +31,6 @@
  */
 function mootimeter_supports($feature) {
     switch ($feature) {
-        case FEATURE_GRADE_HAS_GRADE:
-            return false;
         case FEATURE_MOD_INTRO:
             return true;
         case FEATURE_MOD_PURPOSE:
@@ -100,117 +98,6 @@ function mootimeter_delete_instance($id) {
     }
 
     return $DB->delete_records('mootimeter', ['id' => $id]);
-}
-
-/**
- * Is a given scale used by the instance of mod_mootimeter?
- *
- * This function returns if a scale is being used by one mod_mootimeter
- * if it has support for grading and scales.
- *
- * @param int $moduleinstanceid ID of an instance of this module.
- * @param int $scaleid ID of the scale.
- * @return bool True if the scale is used by the given mod_mootimeter instance.
- */
-function mootimeter_scale_used($moduleinstanceid, $scaleid) {
-    global $DB;
-
-    if ($scaleid && $DB->record_exists('mootimeter', ['id' => $moduleinstanceid, 'grade' => -$scaleid])) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-/**
- * Checks if scale is being used by any instance of mod_mootimeter.
- *
- * This is used to find out if scale used anywhere.
- *
- * @param int $scaleid ID of the scale.
- * @return bool True if the scale is used by any mod_mootimeter instance.
- */
-function mootimeter_scale_used_anywhere($scaleid) {
-    global $DB;
-
-    if ($scaleid && $DB->record_exists('mootimeter', ['grade' => -$scaleid])) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-/**
- * Creates or updates grade item for the given mod_mootimeter instance.
- *
- * Needed by {@see grade_update_mod_grades()}.
- *
- * @param stdClass $moduleinstance Instance object with extra cmidnumber and modname property.
- * @param bool $reset Reset grades in the gradebook.
- * @return void.
- */
-function mootimeter_grade_item_update($moduleinstance, $reset = false) {
-    global $CFG;
-    require_once($CFG->libdir . '/gradelib.php');
-
-    $item = [];
-    $item['itemname'] = clean_param($moduleinstance->name, PARAM_NOTAGS);
-    $item['gradetype'] = GRADE_TYPE_VALUE;
-
-    if ($moduleinstance->grade > 0) {
-        $item['gradetype'] = GRADE_TYPE_VALUE;
-        $item['grademax']  = $moduleinstance->grade;
-        $item['grademin']  = 0;
-    } else if ($moduleinstance->grade < 0) {
-        $item['gradetype'] = GRADE_TYPE_SCALE;
-        $item['scaleid']   = -$moduleinstance->grade;
-    } else {
-        $item['gradetype'] = GRADE_TYPE_NONE;
-    }
-    if ($reset) {
-        $item['reset'] = true;
-    }
-
-    grade_update('/mod/mootimeter', $moduleinstance->course, 'mod', 'mod_mootimeter', $moduleinstance->id, 0, null, $item);
-}
-
-/**
- * Delete grade item for given mod_mootimeter instance.
- *
- * @param stdClass $moduleinstance Instance object.
- * @return grade_item.
- */
-function mootimeter_grade_item_delete($moduleinstance) {
-    global $CFG;
-    require_once($CFG->libdir . '/gradelib.php');
-
-    return grade_update(
-        '/mod/mootimeter',
-        $moduleinstance->course,
-        'mod',
-        'mootimeter',
-        $moduleinstance->id,
-        0,
-        null,
-        ['deleted' => 1]
-    );
-}
-
-/**
- * Update mod_mootimeter grades in the gradebook.
- *
- * Needed by {@see grade_update_mod_grades()}.
- *
- * @param stdClass $moduleinstance Instance object with extra cmidnumber and modname property.
- * @param int $userid Update grade of specific user only, 0 means all participants.
- */
-function mootimeter_update_grades($moduleinstance, $userid = 0) {
-    global $CFG, $DB;
-    require_once($CFG->libdir . '/gradelib.php');
-
-    // Populate array of grade objects indexed by userid.
-    $grades = [];
-    grade_update('/mod/mootimeter', $moduleinstance->course, 'mod', 'mod_mootimeter', $moduleinstance->id, 0, $grades);
 }
 
 /**
