3b24344555f7ecb13b1b90ff8fbc88eaf919c17a
Peter Mayer <peter.mayer@isb.bayern.de>
2024-05-16 20:40:16 +0200

    Fix coding style

--- version.php
+++ version.php
@@ -27,7 +27,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_mootimeter';
 $plugin->release = '0.3.0';
-$plugin->version =  2024051600;
+$plugin->version = 2024051600;
 $plugin->requires = 2022112800;
 $plugin->maturity = MATURITY_ALPHA;
 $plugin->supported = [403, 404]; // A range of branch numbers of supported moodle versions.
